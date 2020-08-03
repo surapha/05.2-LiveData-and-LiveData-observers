1aceca874de186a83da51cffa48e2eb70767eb81
USER PC <surapha.nu@rmuti.ac.th>
2020-08-03 20:38:41 +0700

    7. Task: Add a game-finished event Step 2

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -116,4 +116,7 @@ class GameViewModel : ViewModel() {
     fun onGameFinish() {
         _eventGameFinish.value = true
     }
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
+    }
 }
